282c6fc63ce837658c8569f7d551ed034e96d94b
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-25 15:50:35 +0200

    dev: Added more HTML attribute naems

--- HtmlAttributeNames.cs
+++ HtmlAttributeNames.cs
@@ -29,5 +29,15 @@ namespace NuciWeb
         /// The "src" HTML attribute.
         /// </summary>
         public static string Source => "src";
+
+        /// <summary>
+        /// The "style" HTML attribute.
+        /// </summary>
+        public static string Style => "style";
+
+        /// <summary>
+        /// The "value" HTML attribute.
+        /// </summary>
+        public static string Value => "value";
     }
 }
