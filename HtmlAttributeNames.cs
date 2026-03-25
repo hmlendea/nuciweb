4f862bf3407584b303006eb0cd5fc39e56790400
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-25 13:55:22 +0200

    dev: Created the HtmlAttributeNames class

---
+++ HtmlAttributeNames.cs
@@ -0,0 +1,33 @@
+namespace NuciWeb
+{
+    /// <summary>
+    /// Provides constants for common HTML attribute names.
+    /// </summary>
+    public static class HtmlAttributeNames
+    {
+        /// <summary>
+        /// The "class" HTML attribute.
+        /// </summary>
+        public static string Class => "class";
+
+        /// <summary>
+        /// The "href" HTML attribute.
+        /// </summary>
+        public static string Hyperlink => "href";
+
+        /// <summary>
+        /// The "id" HTML attribute.
+        /// </summary>
+        public static string Id => "id";
+
+        /// <summary>
+        /// The "name" HTML attribute.
+        /// </summary>
+        public static string Name => "name";
+
+        /// <summary>
+        /// The "src" HTML attribute.
+        /// </summary>
+        public static string Source => "src";
+    }
+}
